b57ce9257fb5d8bd390edb6aed61fb5608158991
kumneger Getiye <159164899+assumaqn@users.noreply.github.com>
2025-07-03 20:49:59 +0000

    update

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.html"],
   theme: {
     extend: {},
   },
